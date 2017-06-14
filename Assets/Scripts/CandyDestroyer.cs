601d99702aca656cc0efe745620fe69c8c5ee90a
Nobuhiro Saijo <saijo.magic@gmail.com>
2017-06-14 15:57:35 +0900

    1st commit

---
+++ Assets/Scripts/CandyDestroyer.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandyDestroyer : MonoBehaviour {
+	public CandyHolder candyHolder;
+	public int reward;
+	public GameObject effectPrefab;
+	public Vector3 effectRotation;
+	
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.tag == "Candy"){
+			candyHolder.AddCandy(reward);
+			
+			Destroy(other.gameObject);
+			
+			if(effectPrefab != null){
+				Instantiate(
+					effectPrefab,
+					other.transform.position,
+					Quaternion.Euler(effectRotation)
+				);
+			}
+		}
+	}
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
